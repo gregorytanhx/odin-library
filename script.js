f754e6d246b55a8902497df783413a69dd934049
gregorytanhx <48156960+gregorytanhx@users.noreply.github.com>
2025-01-11 21:55:39 +0800

    improved grid design

--- script.js
+++ script.js
@@ -51,6 +51,10 @@ function deleteBook(event) {
     console.log(idx, myLibrary);
 }
 
+function addBook() {
+
+}
+
 function updateReadStatus(event) {
     let idx = event.target.id.slice(4);
     if (myLibrary[idx].read) {
@@ -100,6 +104,46 @@ const myLibrary = [];
 
 var bookData = [
     {
+        author: 'Samuel Beckett',
+        country: 'Republic of Ireland',
+        imageLink: 'images/molloy-malone-dies-the-unnamable.jpg',
+        language: 'French, English',
+        link: 'https://en.wikipedia.org/wiki/Molloy_(novel)\n',
+        pages: 256,
+        title: 'Molloy, Malone Dies, The Unnamable, the trilogy',
+        year: 1952,
+    },
+    {
+        author: 'Giovanni Boccaccio',
+        country: 'Italy',
+        imageLink: 'images/the-decameron.jpg',
+        language: 'Italian',
+        link: 'https://en.wikipedia.org/wiki/The_Decameron\n',
+        pages: 1024,
+        title: 'The Decameron',
+        year: 1351,
+    },
+    {
+        author: 'Jorge Luis Borges',
+        country: 'Argentina',
+        imageLink: 'images/ficciones.jpg',
+        language: 'Spanish',
+        link: 'https://en.wikipedia.org/wiki/Ficciones\n',
+        pages: 224,
+        title: 'Ficciones',
+        year: 1965,
+    },
+    {
+        author: 'Emily Bront\u00eb',
+        country: 'United Kingdom',
+        imageLink: 'images/wuthering-heights.jpg',
+        language: 'English',
+        link: 'https://en.wikipedia.org/wiki/Wuthering_Heights\n',
+        pages: 342,
+        title: 'Wuthering Heights',
+        year: 1847,
+    },
+    {
         author: 'Jane Austen',
         country: 'United Kingdom',
         imageLink: 'images/pride-and-prejudice.jpg',
@@ -144,5 +188,8 @@ var bookData = [
 for (let i of bookData) {
     addBookToLibrary(new Book(i.title, i.author, i.pages, i.year, i.language));
 }
+
+document.querySelector(".add-book").addEventListener("click", addBook);
+
 
 displayBooks();
